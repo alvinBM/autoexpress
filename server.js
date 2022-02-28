fdb90595c04dcc54c561665a08b2dc226c5f6a78
Alvin Bauma <alvinbauma10@gmail.com>
2022-02-28 16:05:47 +0200

    Config DB

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 import http from "http"
 import app from "./app"
 import 'dotenv/config'
+import database from './config/databases';
+
+// database connexion test
+const testDbConnexion = async() =>{
+    try {
+      await database.authenticate();
+      console.log('Database Successfully Connected');
+    } catch (error) {
+       console.error('Error ff',error);
+    }
+  };
+
+testDbConnexion();
 
 let port = process.env.PORT || 5000;
 
 app.set('port', port);
 
 const server = http.createServer(app);
-
-console.log()
 
 server.listen(port , ()=>{
     console.log(`server run on port::${port}`);
