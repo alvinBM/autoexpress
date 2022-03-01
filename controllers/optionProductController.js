90283091391fe834f874b6fc21e5e8c184161944
Stephan <stephankasole@gmail.com>
2022-03-01 15:11:32 +0100

    creation des methodes option

--- controllers/optionProductController.js
+++ controllers/optionProductController.js
@@ -8,10 +8,35 @@ const optionProductController = {
 
     /**Creer une option */
     creerOption : async (req, res) => {
-        return res.status(200).json({
-            status: 200,
-            message: "Create option"
-        });
+
+
+        /*create: async (req, res) => {*/
+            let { created, option_name, price} = req.body;
+
+            if(!created || !option_name || !price ){
+                return res.status(200).json({
+                    status: 400,
+                    message: "Veuillez remplir tous les champs obligatoire svp"
+                });
+            }
+
+
+            let result = await optionModel.create({created, option_name, price}).then().catch(er => {
+                res.status(200).json({
+                    status: 400,
+                    message: "Impossible de creer  cette option"
+                });
+            });
+
+            if (result) {
+                return res.status(200).json({
+                    status: 200,
+                    message : "Option créé avec succès",
+                    result
+                });
+            }
+            
+       /* },*/
     },
 
 
@@ -19,16 +44,69 @@ const optionProductController = {
      * Recuprrer tous les options
      */
     getOptions : async (req, res) => {
+
+        optionModel.findAll({
+            where:{
+                option_name : option_name
+            },
+            order : [['option_name','DESC']]
+        })
+
+
+        then((data) => {
+            res.status(200).json({
+                status : 200,
+                options : data
+            })
+        }).catch((er) => {
+            res.status(200).json({
+                status : 400,
+                options : null,
+                message : "Impossible de recuperer les options",
+                error : er
+            })
+        });
         return res.status(200).json({
             status: 200,
             message: "get options"
         });
+        
+
     },
 
     /**
      * Recuprrer tous les options
      */
      getOption : async (req, res) => {
+
+        let { id } = req.params;
+            optionModel.findOne({
+                where : {
+                    id : parseInt(id),
+                    option_name : option_name,
+                }
+            }).then((data)=>{
+                if(data){
+                    res.status(200).json({
+                        status : 200,
+                        artwork :data
+                    })
+                }else{
+                    res.status(200).json({
+                        status : 400,
+                        message : "Option introuvable 🙇"
+                    })
+                }
+            }).catch(error => 
+                res.status(200).json({
+                    status : 404,
+                    message : "Impopossible de recuperer cette option 🙇",
+                    error : error
+                })    
+            );
+
+
+
         return res.status(200).json({
             status: 200,
             message: "get option"
