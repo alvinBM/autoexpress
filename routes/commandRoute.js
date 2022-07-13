8de1dde4de4bdba731be38ade26f59b6caeaf7e2
MUHESI <mvmmuhesi@gmail.com>
2022-07-13 18:21:22 +0200

    changes

--- routes/commandRoute.js
+++ routes/commandRoute.js
@@ -8,39 +8,7 @@ router.get("/byStatus/:status", commandCtrl.getCommandByStatus);
 router.put("/:id", commandCtrl.updateStatusCommand);
 router.get("/:id", commandCtrl.getCommande);
 router.get("/user/:id", commandCtrl.getCommandeByUser);
-router.post("/", commandCtrl.createCommande);
+router.post("/", commandCtrl.createCommandV2);
 router.post("/create/", commandCtrl.createCommandV2);
-// router.post("/v2/", commandCtrl.createCommande);
 
 export default router;
-
-// export const commandModel = [
-//   {
-//     amount: 45, // $ 45 total of all panier
-//     user: 8,
-//     created: "07/02/2022",
-//     status: "PAYED",
-//     panier: [
-//       {
-//         nameProduct: "NameOfproduit_1",
-//         product: 1, // id of product
-//         options: "OPTION_1",
-//         quantity: 5,
-//         priceUnit: 2,
-//         total: 10 //
-//       }
-//     ]
-//   }
-// ];
-
-/*   "panier":"[   
-      {
-        "quantity":3,
-        "priceUnit":33,
-        "total":33,
-        "options":"Ok", 
-        "product":1,
-        "nameProduct":"TOYOTA"    
-      }
-    ]"
- */
